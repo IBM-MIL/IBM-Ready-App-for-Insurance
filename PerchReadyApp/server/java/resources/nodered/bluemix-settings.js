87fde969046e8e86553978d941ac61c0f2db40c4
Adam Johns <johnsar@us.ibm.com>
2015-10-23 11:30:04 -0500

    updating bluemix settings file

--- PerchReadyApp/server/java/resources/nodered/bluemix-settings.js
+++ PerchReadyApp/server/java/resources/nodered/bluemix-settings.js
@@ -55,7 +55,7 @@
       cloudantPassword : config.cloudantPassword,
       iotApiKey : config.iotApiKey,
       iotAuthToken : config.iotAuthToken,
-      iotURL : config.iotURL,
+      iotURL : "internetofthings.ibmcloud.com/api/v0001/organizations/",
       iotOrganizationId : config.iotOrganizationId,
       iotDeviceType : "simulatedSensor",
       pushURL : config.pushURL,
